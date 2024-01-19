6c26134d8c0acd8345b818e307cb12a108c16964
Luiz Carlos Bergamini <bergaminiluizcarlos@gmail.com>
2024-01-19 12:14:20 -0300

    Enemy Final Changes
    Finished enemy StateMachine, now the enemy attacks when closer and follows the player, if the player is out of the range, goes back to patrol. Reformulated patrol as well, now it has a predetermined path.

--- Scripts/Enemy StateMachine/Attack.gd
+++ Scripts/Enemy StateMachine/Attack.gd
@@ -1,35 +1,39 @@
 extends EnemyState
-class_name EnemyChase
+class_name EnemyAttack
 
 var player_in_area = false
-var chase_speed
+var patrol_speed = 30
+
+@export var AttackHitCooldown: Timer
+@export var Delay: Timer
+
+var attackSpeed = 5
 
 # Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
 # is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(_msg := {}) -> void:
-	print_debug("Chase")
-	chase_speed = 30
+	print_debug("Attack")
 	player_in_area = true
 	player = get_ancestor(2).player
-	if get_parent().last_state == get_parent().states.get("attack"):
-		chase_speed = 10
+	player.removeHealth(10)
+	AttackHitCooldown.start()
+	
 
 
 # Virtual function. Corresponds to the `_physics_process()` callback.
 func physics_update(delta: float) -> void:
 	if player_in_area:
 		var direction = (player.global_position - enemy.global_position)
-		enemy.velocity = direction * delta * chase_speed
+		enemy.velocity = direction * delta * attackSpeed
 		enemy.move_and_slide()
 
 
-func _on_detection_area_area_exited(area):
-	if area.is_in_group("Player"):
-		player_in_area = false
-		transitioned.emit("patrol")
+func _on_attack_hit_cooldown_timeout():
+	player.removeHealth(10)
 
 
-func _on_detection_area_body_entered(body):
-	if body.is_in_group("Player"):
-		get_ancestor(2).player = body
-		transitioned.emit("Attack")
+func _on_detection_area_body_exited(body):
+	player_in_area = false
+	AttackHitCooldown.stop()
+	transitioned.emit("chase")
+	
--- Scripts/Enemy StateMachine/Patrol.gd
+++ Scripts/Enemy StateMachine/Patrol.gd
@@ -2,7 +2,7 @@ extends EnemyState
 class_name EnemyPatrol
 
 @onready var Pathing = get_ancestor(3)
-@onready var initial_position = enemy.global_position
+@onready var last_position = enemy.global_position
 
 var patrol_speed = 50
 var can_patrol = true
@@ -11,20 +11,23 @@ var can_patrol = true
 # is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(_msg := {}) -> void:
 	print_debug("Patrol")
-	can_patrol = false
 	
 
 
 # Virtual function. Corresponds to the `_physics_process()` callback.
 func physics_update(delta: float) -> void:
-	if enemy.global_position != initial_position and not can_patrol:
-		enemy.global_position.move_toward(initial_position, patrol_speed * delta)
-	else:
-		Pathing.progress += patrol_speed * delta
+	if round(enemy.global_position) != round(last_position) and not can_patrol:
+		enemy.global_position = enemy.global_position.move_toward(last_position, patrol_speed * delta)
+	elif round(enemy.global_position) == round(last_position) and not can_patrol:
 		can_patrol = true
+	elif can_patrol:
+		Pathing.progress += patrol_speed * delta
 
 
 func _on_detection_area_area_entered(area):
 	if area.is_in_group("Player"):
 		get_ancestor(2).player = area.get_parent()
+		can_patrol = false
+		last_position = enemy.global_position
 		transitioned.emit("chase")
+	
